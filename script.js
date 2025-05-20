27c47dfb47eac77672cc68962d309126a370efac
merlinMorgan16 <55897326+merlinMorgan16@users.noreply.github.com>
2025-05-20 14:35:35 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var contentHeight = document.getElementById("internship-content").offsetHeight;
+    var image = document.getElementById("internship-image").querySelector("img");
+    image.style.height = contentHeight + "px";
+});
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var panels = document.getElementsByClassName("panel");
+panels[1].style.display = "block"; 
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        var panels = document.getElementsByClassName("panel");
+        var panel = this.nextElementSibling;
+
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            for (var j = 0; j < panels.length; j++) {
+                panels[j].style.display = "none";
+            }
+            panel.style.display = "block";
+        }
+    });
+}
+
+function checkPassword() {
+    const correctPassword = 'frankbutt';
+    
+    const enteredPassword = document.getElementById('passwordInput').value;
+    
+    const errorMessage = document.getElementById('errorMessage');
+    const transcriptSection = document.getElementById('transcriptSection');
+    
+    if (enteredPassword === correctPassword) {
+        errorMessage.style.display = 'none';
+        
+        transcriptSection.style.display = 'block';
+    } else {
+        errorMessage.style.display = 'block';
+        transcriptSection.style.display = 'none';
+    }
+}
